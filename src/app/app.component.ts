8ee69e393f7d328129b8b06c3910565f90a90ddf
prateek951 <prince.cenation@gmail.com>
2018-05-09 00:19:48 +0530

    working on the sidenav and toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  // openSideNav:boolean = false;
+  // onToggle(){
+  //   this.openSideNav 
+  // }
 }
