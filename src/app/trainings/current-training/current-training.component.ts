c8272ccc5e6c3e86bcad5d225c660ca4fecea601
prateek951 <prince.cenation@gmail.com>
2018-05-09 10:36:12 +0530

    working on the timer,custom events

--- src/app/trainings/current-training/current-training.component.ts
+++ src/app/trainings/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-current-training',
@@ -6,9 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  progress:number = 0;
+  timer:number;
+  @Output() stopTheTraining = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
+    this.timer = setInterval(() => {
+      this.progress += 5;
+      if(this.progress >=100){
+        clearInterval(this.timer);
+      }
+    },1000);
   }
 
+  onStopTraining(){
+    clearInterval(this.timer);
+    // this.stopTheTraining.emit();
+  }
 }
