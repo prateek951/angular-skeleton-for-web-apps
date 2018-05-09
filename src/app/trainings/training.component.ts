931e39b3102ad35bb3ec771dea0393db051b7ec4
prateek951 <prince.cenation@gmail.com>
2018-05-09 10:19:01 +0530

    working on spinner

--- src/app/trainings/training.component.ts
+++ src/app/trainings/training.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./training.component.css']
 })
 export class TrainingComponent implements OnInit {
-
+  onGoingTraining:boolean = false;  
   constructor() { }
 
   ngOnInit() {
